4af890252de365d1f0dce081197829fe661fb722
Vitaliy Zurian <vitaliy.zurian@wkda.de>
2014-12-23 14:58:14 +0100

    Immediate instantination

--- zscroll.js
+++ zscroll.js
@@ -1,43 +1,47 @@
-var Z = Z || {};
+!function () {
+    var Z = {};
+    Z.scroll = {
+        registerScrollEvents: function () {
+            window.onscroll = this.getOnscrollClosure();
+        },
 
-Z.scroll = {
-    registerScrollEvents: function () {
-        window.onscroll = this.getOnscrollClosure();
-    },
+        // TODO: isolate calculations as much as possible
+        getOnscrollClosure: function () {
+            var self = this;
+            var lastPositionY = 0;
+            var lastPositionX = 0;
 
-    // TODO: isolate calculations as much as possible
-    getOnscrollClosure: function () {
-        var lastPositionY = 0;
-        var lastPositionX = 0;
-        var self = this;
+            return function () {
+                var currentPositionY = window.pageYOffset || document.documentElement.scrollTop;
+                var currentPositionX = window.pageXOffset || document.documentElement.scrollLeft;
 
-        return function () {
-            var currentPositionY = window.pageYOffset || document.documentElement.scrollTop;
-            var currentPositionX = window.pageXOffset || document.documentElement.scrollLeft;
+                if (currentPositionY < lastPositionY) {
+                    self.fireEvent('zscroll', {direction: 'up'});
+                }
 
-            if (currentPositionY < lastPositionY) {
-                self.fireEvent('zscroll', {direction: 'up'});
+                if (currentPositionY > lastPositionY) {
+                    self.fireEvent('zscroll', {direction: 'down'});
+                }
+
+                if (currentPositionX < lastPositionX) {
+                    self.fireEvent('zscroll', {direction: 'left'});
+                }
+
+                if (currentPositionX > lastPositionX) {
+                    self.fireEvent('zscroll', {direction: 'right'});
+                }
+
+                lastPositionY = currentPositionY;
+                lastPositionX = currentPositionX;
             }
+        },
 
-            if (currentPositionY > lastPositionY) {
-                self.fireEvent('zscroll', {direction: 'down'});
-            }
-
-            if (currentPositionX < lastPositionX) {
-                self.fireEvent('zscroll', {direction: 'left'});
-            }
-
-            if (currentPositionX > lastPositionX) {
-                self.fireEvent('zscroll', {direction: 'right'});
-            }
-
-            lastPositionY = currentPositionY;
-            lastPositionX = currentPositionX;
+        fireEvent: function (name, data) {
+            var event = new CustomEvent(name, {'detail': data});
+            document.dispatchEvent(event);
         }
-    },
+    };
 
-    fireEvent: function (name, data) {
-        var event = new CustomEvent(name, {'detail': data});
-        document.dispatchEvent(event);
-    }
-};
+    Z.scroll.registerScrollEvents();
+
+}();
